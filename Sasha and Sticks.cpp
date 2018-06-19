ec5ecabd5748075425fdef939b7531cfbce808c4
Mirazul Islam <mirazkuetcse62@gmail.com>
2018-06-19 17:25:08 +0600

    832A	Sasha and Sticks solve in c++

---
+++ Sasha and Sticks.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,k,count=0,ans;
+    cin>>n>>k;
+    ans=n/k;
+    ///cout<<ans;
+    if(ans%2==0)
+        cout<<"NO";
+    else
+        cout<<"YES";
+
+
+}
